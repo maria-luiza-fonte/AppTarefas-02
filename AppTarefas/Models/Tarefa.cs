01d80e085539cca894ca8b9a821734557dda68ad
maria-luiza-fonte <marialuizafonte946@gmail.com>
2025-08-14 14:53:43 -0300

    Adicionar arquivos de projeto.

---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        // Nome da chave primária deve ser Nome da Classe + "Id"
+        public int TarefaId { get; set; } //  Id é a chave primária 
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
