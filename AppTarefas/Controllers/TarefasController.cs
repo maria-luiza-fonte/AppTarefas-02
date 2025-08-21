9d607b06f6791775bc2b4cab35b3e3d0ec8dbd32
maria-luiza-fonte <marialuizafonte946@gmail.com>
2025-08-21 11:53:51 -0300

    Implementação de CRUD para tarefas com melhorias visuais
    Adicionada a classe `Tarefa` e métodos no `TarefasController`
    para gerenciar tarefas em memória, incluindo ações de listar,
    criar, editar, detalhar e excluir. Criadas as páginas de
    visualização (`Index.cshtml`, `Create.cshtml`, `Delete.cshtml`,
    `Details.cshtml`, `Edit.cshtml`) com suporte ao modelo `Tarefa`.
    
    Corrigidos erros de digitação nos arquivos de visualização e
    melhorada a formatação com classes do Bootstrap para melhor
    estilo e usabilidade.

--- AppTarefas/Controllers/TarefasController.cs
+++ AppTarefas/Controllers/TarefasController.cs
@@ -1,12 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppTarefas.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppTarefas.Controllers
 {
     public class TarefasController : Controller
     {
+        private static List<Tarefa> _tarefas = new List<Tarefa>();
+        private static int proximoId = 1;
         public IActionResult Index()
+        {
+            return View(_tarefas);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                tarefa.TarefaId = proximoId++;
+                _tarefas.Add(tarefa);
+                return RedirectToAction("Index");
+            }
+            return View(tarefa);
+        }
+        public IActionResult Edit(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Tarefa tarefaAtualizada)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+
+            tarefa.Titulo = tarefaAtualizada.Titulo;
+            tarefa.Descricao = tarefaAtualizada.Descricao;
+            tarefa.Concluida = tarefaAtualizada.Concluida;
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefa != null)
+            {
+                _tarefas.Remove(tarefa);
+            }
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
